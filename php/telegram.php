6ae79c093d522cfb5ec8a6116d6ed80994a9e607
Martynenko <4lexm4rtin@gmail.com>
2020-08-20 04:47:42 +0300

    all add

--- php/telegram.php
+++ php/telegram.php
@@ -1,36 +1,50 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // $sendToTelegram = fopen("https://api.telegram.org/bot1255735512:AAH4YmGBAMMeIVYwGpNlQGiHiTj1KbmKWvo/sendMessage?chat_id=-392300022&parse_mode=html&text=555","r");
-if (!empty($_POST['user_name']) && !empty($_POST['user_phone'])){
-  if (isset($_POST['user_name'])) {
-    if (!empty($_POST['user_name'])){
-  $name = strip_tags($_POST['user_name']);
-  $nameFieldset = "Имя: ";
-  }
-}
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//   // $sendToTelegram = fopen("https://api.telegram.org/bot1255735512:AAH4YmGBAMMeIVYwGpNlQGiHiTj1KbmKWvo/sendMessage?chat_id=-392300022&parse_mode=html&text=555","r");
+// if (!empty($_POST['user_name']) && !empty($_POST['user_phone'])){
+//   if (isset($_POST['user_name'])) {
+//     if (!empty($_POST['user_name'])){
+//   $name = strip_tags($_POST['user_name']);
+//   $nameFieldset = "Имя: ";
+//   }
+// }
  
-if (isset($_POST['user_phone'])) {
-  if (!empty($_POST['user_phone'])){
-  $phone = strip_tags($_POST['user_phone']);
-  $phoneFieldset = "Телефон: ";
-  }
-}
-if (isset($_POST['user_email'])) {
-  if (!empty($_POST['user_email'])){
-  $theme = strip_tags($_POST['user_email']);
-  $themeFieldset = "Почта: ";
-  }
-}
+// if (isset($_POST['user_phone'])) {
+//   if (!empty($_POST['user_phone'])){
+//   $phone = strip_tags($_POST['user_phone']);
+//   $phoneFieldset = "Телефон: ";
+//   }
+// }
+// if (isset($_POST['user_email'])) {
+//   if (!empty($_POST['user_email'])){
+//   $theme = strip_tags($_POST['user_email']);
+//   $themeFieldset = "Почта: ";
+//   }
+// }
+// $token = "1255735512:AAH4YmGBAMMeIVYwGpNlQGiHiTj1KbmKWvo";
+// $chat_id = "-392300022";
+// $arr = array(
+//   $nameFieldset => $name,
+//   $phoneFieldset => $phone,
+//   $themeFieldset => $theme
+// );
+// foreach($arr as $key => $value) {
+//   $txt .= "<b>".$key."</b> ".$value."%0A";
+// };
+
 $token = "1255735512:AAH4YmGBAMMeIVYwGpNlQGiHiTj1KbmKWvo";
 $chat_id = "-392300022";
+
 $arr = array(
-  $nameFieldset => $name,
-  $phoneFieldset => $phone,
-  $themeFieldset => $theme
+  "Имя: " => $_POST['user_name'],
+  "Телефон: " => $_POST['user_phone'],
+  "Почта: " => $_POST['user_email']
 );
+
 foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
+    $txt .= "<b>".$key."</b> ".$value."%0A";
 };
+
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 if ($sendToTelegram) {
   
@@ -39,12 +53,12 @@ if ($sendToTelegram) {
 } else {
   echo '<p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p>';
 }
-} else {
-  echo '<p class="fail">Ошибка. Вы заполнили не все обязательные поля!</p>';
-}
-} else {
-header ("Location: /");
-}
+// } else {
+//   echo '<p class="fail">Ошибка. Вы заполнили не все обязательные поля!</p>';
+// }
+// } else {
+// header ("Location: /");
+// }
 
 // $token = "1255735512:AAH4YmGBAMMeIVYwGpNlQGiHiTj1KbmKWvo";
 // $chat_id = "-392300022";
