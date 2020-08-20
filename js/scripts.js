fb0a5d0545974df5d3b774c7f788393a2c786537
Martynenko <4lexm4rtin@gmail.com>
2020-08-20 04:48:35 +0300

    all add

--- js/scripts.js
+++ js/scripts.js
@@ -4330,37 +4330,37 @@ window.addEventListener("DOMContentLoaded", function () {
 //   document.location='http://4lexm4rtin.byethost14.com/';
 //   return false;
 // }
-$(document).ready(function () {
-  $(".form").submit(function () {
-      var formID = $(this).attr('id');
-      var formNm = $('#' + formID);
-      var message = $(formNm).find(".form-message");
-      var formTitle = $(formNm).find(".form-title");
-      $.ajax({
-          type: "POST",
-          url: '../php/telegram.php',
-          data: formNm.serialize(),
-          success: function (data) {
-            // Вывод сообщения об успешной отправке
-            message.html(data);
-            formTitle.css("display","none");
-            setTimeout(function(){
-              formTitle.css("display","block");
-              message.html('');
-              $('input').not(':input[type=submit], :input[type=hidden]').val('');
-            }, 3000);
-          },
-          error: function (jqXHR, text, error) {
-              // Вывод сообщения об ошибке отправки
-              message.html(error);
-              formTitle.css("display","none");
-              setTimeout(function(){
-                formTitle.css("display","block");
-                message.html('');
-                $('input').not(':input[type=submit], :input[type=hidden]').val('');
-              }, 3000);
-          }
-      });
-      return false;
-  });
-});
+// $(document).ready(function () {
+//   $(".form").submit(function () {
+//       var formID = $(this).attr('id');
+//       var formNm = $('#' + formID);
+//       var message = $(formNm).find(".form-message");
+//       var formTitle = $(formNm).find(".form-title");
+//       $.ajax({
+//           type: "POST",
+//           url: '../php/telegram.php',
+//           data: formNm.serialize(),
+//           success: function (data) {
+//             // Вывод сообщения об успешной отправке
+//             message.html(data);
+//             formTitle.css("display","none");
+//             setTimeout(function(){
+//               formTitle.css("display","block");
+//               message.html('');
+//               $('input').not(':input[type=submit], :input[type=hidden]').val('');
+//             }, 3000);
+//           },
+//           error: function (jqXHR, text, error) {
+//               // Вывод сообщения об ошибке отправки
+//               message.html(error);
+//               formTitle.css("display","none");
+//               setTimeout(function(){
+//                 formTitle.css("display","block");
+//                 message.html('');
+//                 $('input').not(':input[type=submit], :input[type=hidden]').val('');
+//               }, 3000);
+//           }
+//       });
+//       return false;
+//   });
+// });
